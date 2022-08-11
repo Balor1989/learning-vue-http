c50f1ea850161999946f8139f301ee810eedf826
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-11 17:05:54 +0300

    use custom directives

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,16 @@
   <div class="container">
     <AppBlock />
     <div class="card">
-      <h2 v-color="myColor">Directives</h2>
+      <h2 v-color:[type].blink="myColor">Blink</h2>
+      <h2 v-color:[type].hover="myColor">Hover</h2>
       <button class="btn" @click="myColor = 'blue'">Change color</button>
+      <button
+        class="btn"
+        @click="type = type === 'color' ? 'backgroundColor' : 'color'"
+      >
+        Change type
+      </button>
+      {{ type }}
     </div>
     <form action="" class="form-control card">
       <label for="test"> test</label>
@@ -20,6 +28,7 @@ export default {
   data() {
     return {
       myColor: "green",
+      type: "color",
     };
   },
   components: {
--- src/directives/colorDirective.js
+++ src/directives/colorDirective.js
@@ -1,11 +1,41 @@
+let interval = null;
+
+let defaultColor;
+
+const mouseover = (event) => {
+  event.target.style.color = "#000";
+};
+const mouseout = (event) => {
+  event.target.style.color = defaultColor;
+};
+
 export default {
   mounted(el, binding) {
+    defaultColor = binding.value;
     console.log(el);
     console.log(binding);
-    el.style.color = binding.value;
+    el.style[binding.arg] = binding.value;
+
+    if (binding.modifiers.blink) {
+      let flag = true;
+      interval = setInterval(() => {
+        el.style.color = flag ? "#fff" : binding.value;
+        flag = !flag;
+      }, 1000);
+    }
+    if (binding.modifiers.hover) {
+      el.addEventListener("mouseover", mouseover);
+      el.addEventListener("mouseout", mouseout);
+    }
   },
   updated(el, binding) {
-    el.style.color = binding.value;
+    el.style[binding.arg] = binding.value;
   },
-  unmounted() {},
+  unmounted(el) {
+    if (interval) {
+      clearInterval(interval);
+      el.removeEventListener("mouseover", mouseover);
+      el.removeEventListener("mouseout", mouseout);
+    }
+  },
 };
